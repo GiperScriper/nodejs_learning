b58f532ced98335505b949262e89cdac8c52c56b
Nikolay Martynov <tty0sh@gmail.com>
2014-10-14 12:48:10 +0400

    modified server.js to echo server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url');
 
-var server = new http.Server(); // EventEmitter
+var server = new http.Server(function(req, res) {
+    //console.log(req.headers);
+    console.log(req.method, req.url);
+
+    var urlParsed = url.parse(req.url, true);
+    //console.log(urlParsed);
+
+    if ( urlParsed.pathname == '/echo' && urlParsed.query.message ) {
+        res.end( urlParsed.query.message );
+    } else {
+        res.statusCode = 404;
+        res.end('incorrect query!');
+    };
+    
+}); // EventEmitter
 
 server.listen(1337, 'localhost');
-
-var counter = 0;
-
-server.on('request', function(req, res){
-    res.end('hello, world! ' + ++counter);
-});
