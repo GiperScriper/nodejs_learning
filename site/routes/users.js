2ececd53b5470161fecc148e0eb2997a9e82f621
GiperScriper <tty0sh@gmail.com>
2014-10-28 23:06:12 +0200

    fix POST handler

--- site/routes/users.js
+++ site/routes/users.js
@@ -3,23 +3,26 @@ var router = express.Router();
 var User = require('../schema/user').User;
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     User.find({}, function (err, data) {
         if (err) throw err;
         res.render('users', { users: data });
     });
-    next();
+});
+
+
+/* Test list */
+router.get('/test', function (req, res) {
+	res.json({message: "from /users/test"});
 });
 
 
 /* POST save user to database */
-router.post('/', function (req, res) {
+router.post('/', function (req, res) {	
 	var user = new User({
-		name: req.param.name,
-		password: req.param.password
-	});
-
-	console.log('POST request');
+		name: req.param('name'),
+		password: req.param('password')
+	});	
 
 	user.save(function (err) {
 		if (err) throw error;
