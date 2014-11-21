445cfdc64d610b0236794852d1bdada75bd9478d
Nikolay Martynov <tty0sh@gmail.com>
2014-11-21 11:14:26 +0400

    few changes in app.js (angular app)

--- site/public/js/app/app.js
+++ site/public/js/app/app.js
@@ -1,13 +1,13 @@
 //(function (){
-    "use strict";
+"use strict";
 
-	var app = angular.module('app', ['ngResource', 'ngRoute']);
+    var app = angular.module('app', ['ngResource', 'ngRoute']);
 
-	// app.config(function($routeProvider, $locationProvider){
-	// 	$locationProvider.html5Mode(true);
-	// 	$routeProvider
-	// 		.when('/', { templateUrl: 'index' , controller: 'mainCtrl'})
-	// });
+    // app.config(function($routeProvider, $locationProvider){
+    // 	$locationProvider.html5Mode(true);
+    // 	$routeProvider
+    // 		.when('/', { templateUrl: 'index' , controller: 'mainCtrl'})
+    // });
     
     app.controller('mainCtrl', function ($scope){
         $scope.myVar = 'Hello Angular!';
