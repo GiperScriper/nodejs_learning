d4e4702bf051676931793d94e79dd78b04e55c39
GiperScriper <tty0sh@gmail.com>
2014-11-27 20:54:57 +0200

    listing contacts in Contacts Angular App

--- site/public/js/contact_app/app.js
+++ site/public/js/contact_app/app.js
@@ -2,25 +2,20 @@
 
 var app = angular.module('app', ['ngRoute', 'ngResource']);
 
+
 app.config(function ($routeProvider, $locationProvider) {
     
     // avoid hash-bang in url
     $locationProvider.html5Mode(true);
     
-    //routes for Contacts page
+    //routes for Contacts App
     $routeProvider
-     .when('/contacts/new', { 
+        .when('/contacts', {
+            templateUrl: 'js/contact_app/templates/list.html',
+            controller: 'ContactsCtrl'
+        })
+        .when('/contacts/new', { 
             templateUrl: 'js/contact_app/templates/newContact.html', 
             controller: 'newContactCtrl'
         })            
-});
-
-
-app.controller('mainCtrl', function ($scope) {
-    $scope.message = 'Angular contact app';
-});
-
-
-app.controller('newContactCtrl', function ($scope) {
-    $scope.message = 'new contact app';
 });
